e91093de6b1b37832c9203be843f5b6a3796b312
Mattias Wallerman <wallermanmattias@yahoo.se>
2021-06-22 06:29:22 +0200

    expression body syntax vs. block syntax

--- ConsoleApp3/Vehicle.cs
+++ ConsoleApp3/Vehicle.cs
@@ -11,9 +11,9 @@ namespace ConsoleApp3
         {
             this.RegNo = RegNo;
         }
-        public string Drive(double distance)
-        {
-
-        }
+        public string Drive(double distance) => distance > 0 ? $"Vehicle wants to drive for {distance}" // expression body syntax 
+        /*{
+           block syntax
+        }*/
     }
 }
