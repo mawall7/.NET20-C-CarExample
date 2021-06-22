c23775f861b6e43eaaca55cc04298da9658ddf96
Mattias Wallerman <wallermanmattias@yahoo.se>
2021-06-22 09:56:59 +0200

    List<Vehicle>, Objekt instantierare

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ConsoleApp3
 {
@@ -8,7 +9,13 @@ namespace ConsoleApp3
         {
             Class2 class2 = new Class2();
             var fuelVehicle = new fuelVehicle("ABC12A", 20);
-            
+
+            var list = new List<Vehicle>()
+            {
+                new Vehicle("ABC123"),
+                new Vehicle("ABC132"),
+                new fuelVehicle("ABD124",10.2)
+            };
             
         }
     }
